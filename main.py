77ba4d0f01376f7dec05daf2348be4c42c337792
maribor666 <spekaoleh@ukr.net>
2018-11-09 17:39:31 +0200

    add:part2 testing

--- main.py
+++ main.py
@@ -1,16 +1,84 @@
 from pprint import pprint
 import csv
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+import random
+
 
 data_path = "./resources/winequality-red.csv"
 
-
+# для v.2 брати x = density y= citric acidity
 def main():
     reader = csv.reader(open(data_path, mode='r'), delimiter=";")
     data = [el for el in reader]
-    pprint(data[0])
+    # for el in data:
+    #     print(el)
     # plot_scatter_matrix(data, 7, 4)
+    part2(data)
+
+
+def ft_dot(a, b):
+    if len(a) != len(b):
+        raise ValueError
+    return sum([ai * bi for ai, bi in zip(a, b)])
+
+def part2(data):
+    X = []
+    y= []
+    for line in data[1:]:
+        if float(line[-1]) >= 8 or float(line[-1]) <= 3:
+            if float(line[-1]) >= 8:
+                mark = 1
+            if float(line[-1]) <= 3:
+                mark = 0
+            y.append(mark)
+            x = [float(line[10]), float(line[8])]
+            X.append(x)
+    print(X)
+    print(y)
+    perc = Perceptron(lr=0.0005)
+    perc.train(X, y)
+
+
+class Perceptron:
+
+    def __init__(self, lr=0.0001):
+        self.lr = lr
+        self.performance = []
+        self.W = []
+        self.bias = None
+
+    def _heaviside(self, arg):
+        return 1 if arg >= 0 else 0
+
+    def _train_epoch(self, X, y):
+        errors_num = 0
+        for xi, yi in zip(X, y):
+            res = ft_dot(self.W, xi) + self.bias
+            if self._heaviside(res) != yi:
+                errors_num += 1
+                self.bias = self.bias + self.lr * (yi - self._heaviside(res))
+                Wnew = []
+                for w in self.W:
+                    wnew = w + self.lr * (yi - self._heaviside(res))
+                    Wnew.append(wnew)
+                self.W = Wnew
+                # print('Wnew', Wnew, self.bias)
+        return errors_num
+
+
+    def train(self, X, y, epoches=0):
+        self.W = [random.uniform(-1, 1) for i in range(len(X[0]))]
+        self.bias = random.uniform(-1, 1)
+        print(self.W, self.bias)
+        epoch_num = 0
+        while True:
+            epoch_erro_num = self._train_epoch(X, y)
+            epoch_num += 1
+            print(epoch_num, epoch_erro_num, self.W, self.bias)
+            if epoch_erro_num == 0:
+                break
+
+
 
 
 
